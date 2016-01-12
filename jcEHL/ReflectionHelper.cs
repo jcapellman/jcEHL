c18eb89fbc917c5da44687383403919fa775a6f3
Jarred Capellman <jcapellman@hotmail.com>
2016-01-11 20:35:52 -0500

    Added ReflectionHelper

---
+++ jcEHL/ReflectionHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace jcEHL {
+    public class ReflectionHelper {
+        public T FindTInAssembly<T>(object implementationValue, string implementationProperty) where T : class {
+            var assembly = (Assembly)typeof(Assembly).GetTypeInfo().GetDeclaredMethod("GetCallingAssembly").Invoke(null, new object[0]);
+
+            var types = assembly.DefinedTypes.ToList();
+
+            foreach (var type in types) {
+                if (type.IsSubclassOf(typeof(T)) && !type.IsAbstract) {
+                    var obj = (T)Activator.CreateInstance(type.AsType());
+                    
+                    if (obj.GetType().GetRuntimeField(implementationProperty).GetValue(obj) == implementationValue) {
+                        return obj;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
